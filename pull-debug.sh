e798c107d06bcec79753b6b63a2ed88f3d03da8b
Santosh Singh <santosh.singh@surya-solutions.com>
2016-07-06 19:33:28 +0100

    comments:

--- pull-debug.sh
+++ pull-debug.sh
@@ -1,4 +1,4 @@
 git reset --hard
 git pull
 if pgrep nodejs &> /dev/null ; then pkill -9  nodejs ; fi
-ROOT=/home/santosh/homebee/homebee-server-app/www PORT=4000 HTTPSPORT=4043 nodejs ./bin/www --log=debug --dbdebug=true --homebeeserver >> node.log 2>&1 &
+ROOT=/home/santosh/homebee/homebee-server-app/platforms/browser/www PORT=4000 HTTPSPORT=4043 nodejs ./bin/www --log=debug --dbdebug=true --homebeeserver >> node.log 2>&1 &
