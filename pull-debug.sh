5cf35bf4b8094a5f0820cc4b9ba37bfc84ab2bd7
Santosh Singh <santosh.singh@surya-solutions.com>
2016-07-06 18:12:41 +0100

    comments:

--- pull-debug.sh
+++ pull-debug.sh
@@ -1,5 +1,4 @@
 git reset --hard
 git pull
 if pgrep nodejs &> /dev/null ; then pkill -9  nodejs ; fi
-nodejs ./bin/www --log=debug --dbdebug=true --homebeeserver >> node.log 2>&1 &
-
+PORT=4000 HTTPSPORT=4043 nodejs ./bin/www --log=debug --dbdebug=true --homebeeserver >> node.log 2>&1 &
