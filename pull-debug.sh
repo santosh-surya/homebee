021f660ee9564bea293c047aa0eb2b057996fab2
Santosh Singh <santosh.singh@surya-solutions.com>
2016-07-07 12:53:49 +0100

    comments:

--- pull-debug.sh
+++ pull-debug.sh
@@ -1,4 +1,7 @@
 git reset --hard
 git pull
-kill `pgrep  -f "nodejs.*--homebeeserver*"`
+echo "Repository refreshed ... now killing old process"
+if pgrep  -f "nodejs.*--homebeeserver*" &> /dev/null ; then pkill -9  `pgrep  -f "nodejs.*--homebeeserver*"` ; fi;
+echo "Starting HomeBee Server now ..."
 ROOT=/home/santosh/homebee/homebee-server-app/platforms/browser/www PORT=4000 HTTPSPORT=4043 nodejs ./bin/www --log=debug --dbdebug=true --homebeeserver >> node.log 2>&1 &
+tail -f node.log
