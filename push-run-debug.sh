17ac7ff53c0ef67924ed347b764a81418df3174b
Santosh Singh <santosh.singh@surya-solutions.com>
2016-07-08 11:23:29 +0100

    comments:

--- push-run-debug.sh
+++ push-run-debug.sh
@@ -1,5 +1,4 @@
 git add .
 git commit -a -m "comments: $1"
 git push
-ssh santosh@rackspace "cd /home/santosh/homebee/homebee-server;git reset --hard;git pull;echo "Repository refreshed ... now killing old process";if pgrep  -f "nodejs.*--homebeeserver*" &> /dev/null ; then kill -9  `pgrep  -f "nodejs.*--homebeeserver*"` ; fi;echo "Starting HomeBee Server now ...";ROOT=/home/santosh/homebee/homebee-server-app/platforms/browser/www PORT=4000 HTTPSPORT=4043 nodejs ./bin/www --log=debug --dbdebug=true --homebeeserver >> node.log 2>&1 &;tail -f node.log
-"
+ssh santosh@rackspace "cd /home/santosh/homebee/homebee-server;git reset --hard;git pull;echo \"Repository refreshed ... now killing old process\";if pgrep  -f \"nodejs.*--homebeeserver*\" &> /dev/null ; then kill -9  `pgrep  -f \"nodejs.*--homebeeserver*\"` ; fi;echo \"Starting HomeBee Server now ...\";ROOT=/home/santosh/homebee/homebee-server-app/platforms/browser/www PORT=4000 HTTPSPORT=4043 nodejs ./bin/www --log=debug --dbdebug=true --homebeeserver >> node.log 2>&1 &;tail -f node.log;"
