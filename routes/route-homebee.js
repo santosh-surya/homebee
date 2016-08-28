ce2d965201a326fbd468922d1ffcafe734abd635
Santosh Singh <santosh.singh@surya-solutions.com>
2016-08-28 18:22:06 +0100

    comments:

--- routes/route-homebee.js
+++ routes/route-homebee.js
@@ -5,5 +5,6 @@ var homebeeapi = require('../controllers/homebee-api-controller');
 router.use('/register', homebeeapi.authorise, homebeeapi.register);
 router.use('/login', homebeeapi.authorise, homebeeapi.login);
 router.use('/user-devices', homebeeapi.authorise, homebeeapi.userDevices);
+router.use('/ping', homebeeapi.authorise, homebeeapi.ping);
 
 module.exports = router;
